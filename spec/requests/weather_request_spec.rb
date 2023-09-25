85fdae55a3dc93d8716e947fe33cfe97d6773eee
Ian Lyell <ian.lyell@yahoo.com>
2023-09-24 22:13:12 -0500

    test: Add weather request spec

---
+++ spec/requests/weather_request_spec.rb
@@ -0,0 +1,49 @@
+require 'rails_helper'
+
+RSpec.describe Weather, type: :request do
+    it 'sends current weather data for a city' do
+        get api_v0_weather, params: { city: 'new orleans', state: 'LA' }
+
+        expect(response).to be_successful
+
+        weather = JSON.parse(response.body, symbolize_names: true)
+
+        expect(weather).to have_key(:data)
+
+        expect(weather[:data]).to have_key(:id)
+        expect(weather[:data][:type]).to eq("forecast")
+        expect(weather[:data]).to have_key(:attributes)
+
+        expect(weather[:data][:attributes]).to have_key(:current_weather)
+        expect(weather[:data][:attributes]).to have_key(:daily_weather)
+        expect(weather[:data][:attributes]).to have_key(:hourly_weather)
+        
+        expect(weather[:data][:attributes][:current_weather]).to have_key(:last_updated)
+        expect(weather[:data][:attributes][:current_weather]).to have_key(:temperature)
+        expect(weather[:data][:attributes][:current_weather]).to have_key(:feels_like)
+        expect(weather[:data][:attributes][:current_weather]).to have_key(:humidity)
+        expect(weather[:data][:attributes][:current_weather]).to have_key(:uvi)
+        expect(weather[:data][:attributes][:current_weather]).to have_key(:visibility)
+        expect(weather[:data][:attributes][:current_weather]).to have_key(:condition)
+        expect(weather[:data][:attributes][:current_weather]).to have_key(:icon)
+
+        expect(weather[:data][:attributes][:daily_weather]).to be_a(Array)
+        expect(weather[:data][:attributes][:daily_weather].count).to eq(5)
+
+        expect(weather[:data][:attributes][:daily_weather][0]).to have_key(:date)
+        expect(weather[:data][:attributes][:daily_weather][0]).to have_key(:sunrise)
+        expect(weather[:data][:attributes][:daily_weather][0]).to have_key(:sunset)
+        expect(weather[:data][:attributes][:daily_weather][0]).to have_key(:max_temp)
+        expect(weather[:data][:attributes][:daily_weather][0]).to have_key(:min_temp)
+        expect(weather[:data][:attributes][:daily_weather][0]).to have_key(:condition)
+        expect(weather[:data][:attributes][:daily_weather][0]).to have_key(:icon)
+
+        expect(weather[:data][:attributes][:hourly_weather]).to be_a(Array)
+        expect(weather[:data][:attributes][:hourly_weather].count).to eq(24)
+
+        expect(weather[:data][:attributes][:hourly_weather][0]).to have_key(:time)
+        expect(weather[:data][:attributes][:hourly_weather][0]).to have_key(:temperature)
+        expect(weather[:data][:attributes][:hourly_weather][0]).to have_key(:condition)
+        expect(weather[:data][:attributes][:hourly_weather][0]).to have_key(:icon)
+    end
+end
--- spec/services/weather_service_spec.rb
+++ spec/services/weather_service_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe 'Weather Service' do
     describe 'instance methods' do
         it 'returns 5 day forecast for a given lat and lon.', :vcr do
             service = WeatherService.new
-            forecast = service.get_forecast(29.9537,-90.07775)
+            forecast = service.get_forecast("new orleans","la")
             expect(forecast).to have_key(:location)
             expect(forecast[:location]).to have_key(:lat)
             expect(forecast[:location]).to have_key(:lon)
